ae3e0ee2467cbcd41697944a1dca8e3e3a2bb4b2
klaus-liebler <mail@klaus-liebler.de>
2016-07-11 15:06:02 +0200

    sensactSE

--- firmware/sensactSe/src/application.cpp
+++ firmware/sensactSe/src/application.cpp
@@ -4,7 +4,7 @@
 #include "stm32f0xx_hal_uart.h"
 
 #define TIMER TIM16
-static const uint16_t TIMER_RESET = 1000;
+static const uint16_t MINIMUM_RESET_TIME = 1000;
 static const uint16_t TIMER_WAIT_FOR_OTHER_PRESENSE = 1000;
 static const uint16_t TIMER_WAIT_FOR_ME_RELEASE_PRESENCE = 1000;
 static const uint16_t TIMER_WAIT_FOR_ALL_RELEASE_PRESENCE = 1000;
@@ -13,10 +13,28 @@ static uint32_t GetExactTime()
 	return 0;
 }
 
+static inline void Release()
+{
+	HAL_GPIO_WritePin(OneWire_GPIO_Port, OneWire_Pin, GPIO_PIN_SET);
+}
+
+static inline void PullDown()
+{
+	HAL_GPIO_WritePin(OneWire_GPIO_Port, OneWire_Pin, GPIO_PIN_RESET);
+}
+
+static inline bool Probe()
+{
+	return HAL_GPIO_ReadPin(OneWire_GPIO_Port, OneWire_Pin)==GPIO_PIN_SET;
+}
 static void FireTimerIn(uint16_t timeunits)
 {
 	TIMER->ARR = timeunits;
 	SET_BIT(TIMER->CR1, TIM_CR1_CEN);
+}
+static void TimerOff()
+{
+	CLEAR_BIT(TIMER->CR1, TIM_CR1_CEN);
 }
 
 namespace sensact
@@ -35,32 +53,29 @@ uint32_t cApplication::lastFallingEdge=0;
 void cApplication::Run()
 {
 	Console::Writeln("Application started");
-	while(1)
-		{
-			HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_4);
-			HAL_Delay(200);
-			uint32_t isr= USART1->ISR;
-			uint16_t rdr= USART1->RDR;
-		}
 	//warte, bin gerade kein pulldown erfolgt
 	while(!HAL_GPIO_ReadPin(OneWire_GPIO_Port, OneWire_Pin));
 	//Preload disabled - das Setzen von ARR wird unmittelbar berücksichtigt
 	CLEAR_BIT(TIMER->CR1, TIM_CR1_ARPE); //TIMx_ARR register is not buffered!
 	state=eState::WAIT_FOR_RESET;
-	while(true)
-	{
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_4);
-		HAL_Delay(500);
-	}
-
+	lastFallingEdge=0;
 }
 
 //Merke: Jedes PullDown könnte ein Reset sein
+//TODO: Hier findet ein Wechselspiel zwischen Timer und EXTI-Interrupts statt
+//Beide Interrupts müssen die gleiche Priorität haben, damit sie sich nicht gegenseitig unterbrechen können
+//Beide Interrupts müssen als letzte Aktion den anderen clearen/resetten, damit kein "alter" Zustand ein "neues" Interrupthandling auslösen kann
+
+//Probiere aus:
+//Warte, bis variable auf 2:
+//EXTI: ISR deaktivieren, lampe toggeln, 100msek verzögerung , variable++, ISR aktivieren, return
+//taster muss gedrückt und wieder losgelassen werden
+//FireTimer(1000)
 
 void cApplication::OnOneWireInterrupt()
 {
 	Console::Writeln("OnOneWireInterrupt called");
-	GPIO_PinState pinstate = HAL_GPIO_ReadPin(OneWire_GPIO_Port, OneWire_Pin);
+	bool pinstate = Probe();
 	switch (state) {
 		case eState::UNINITIALIZED:
 			return;
@@ -72,12 +87,15 @@ void cApplication::OnOneWireInterrupt()
 			}
 			//pin ist low
 			state = eState::MINIMUM_RESET_TIME_IS_NOT_OVER;
-			FireTimerIn(TIMER_RESET);
+			FireTimerIn(MINIMUM_RESET_TIME);
+			lastFallingEdge=GetExactTime();
 			break;
 		case eState::MINIMUM_RESET_TIME_IS_NOT_OVER:
 			if(!pinstate)
 			{
+				//Das kann nicht sein, weil er hier nur bei einem High-Level reinkommen könnte
 				Console::Writeln("In state MINIMUM_RESET_TIME_IS_NOT_OVER, but low level!");
+				while(1);
 			}
 			state=eState::WAIT_FOR_RESET;
 			break;
@@ -89,12 +107,12 @@ void cApplication::OnOneWireInterrupt()
 			}
 			else
 			{
-				state=eState::WAIT_FOR_OTHER_PRESENSE;
+				state=eState::WAIT_FOR_APPLY_PRESENCE;
 				FireTimerIn(TIMER_WAIT_FOR_OTHER_PRESENSE);
 			}
 			break;
-		case eState::WAIT_FOR_OTHER_PRESENSE:
-			//Wenn FF-Interrupt feuert und wir uns in WAIT_FOR_OTHER_PRESENSE befinden -> lastFallingEdge merken und in status OTHER_PRESENCE_DETECTED
+		case eState::WAIT_FOR_APPLY_PRESENCE:
+			//Wenn FF-Interrupt feuert und wir uns in WAIT_FOR_APPLY_PRESENSE befinden -> lastFallingEdge merken und in status OTHER_PRESENCE_DETECTED
 			if(pinstate)
 			{
 				Console::Writeln("high in WAIT_FOR_OTHER_PRESENSE");
@@ -128,13 +146,13 @@ void cApplication::OnTimerInterrupt()
 		case eState::MINIMUM_RESET_TIME_IS_NOT_OVER:
 			state=eState::MINIMUM_RESET_TIME_IS_OVER;
 			break;
-		case eState::WAIT_FOR_OTHER_PRESENSE:
-			HAL_GPIO_WritePin(OneWire_GPIO_Port, OneWire_Pin, GPIO_PIN_RESET);
+		case eState::WAIT_FOR_APPLY_PRESENCE:
+			PullDown();
 			state=eState::WAIT_FOR_ME_RELEASE_PRESENCE;
 			FireTimerIn(TIMER_WAIT_FOR_ME_RELEASE_PRESENCE);
 			break;
 		case eState::WAIT_FOR_ME_RELEASE_PRESENCE:
-			HAL_GPIO_WritePin(OneWire_GPIO_Port, OneWire_Pin, GPIO_PIN_SET);
+			Release();
 			state=eState::WAIT_FOR_ME_RELEASE_PRESENCE;
 			FireTimerIn(TIMER_WAIT_FOR_ALL_RELEASE_PRESENCE);
 			break;
