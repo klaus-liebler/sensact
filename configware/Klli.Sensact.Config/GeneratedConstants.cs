391eef6979b104a9d56721f8f4efe0de2c70449a
klaus-liebler <mail@klaus-liebler.de>
2016-08-16 21:51:55 +0200

    Delete GeneratedConstants.cs

--- configware/Klli.Sensact.Config/GeneratedConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace Klli.Sensact.Config
-{
-    public static class GeneratedConstants
-    {
-        public const string ConfigSHA = /*ReplaceConfigSHA*/"26e97e48ab0fe9374b6c764650c1b11841796800"/*ReplaceConfigSHA*/;
-    }
-}
