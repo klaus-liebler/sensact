c75b82f30bef80cfcfb054b369407f7fa38cfec2
klaus-liebler <mail@klaus-liebler.de>
2016-08-18 21:48:35 +0200

    Added GeneratedConstants

---
+++ configware/Klli.Sensact.Config/GeneratedConstants.cs
@@ -0,0 +1,7 @@
+namespace Klli.Sensact.Config
+{
+    public static class GeneratedConstants
+    {
+        public const string ConfigSHA = /*ReplaceConfigSHA*/"8ce57aac2ab6586766979f020d5f8ea97ca345c0"/*ReplaceConfigSHA*/;
+    }
+}
